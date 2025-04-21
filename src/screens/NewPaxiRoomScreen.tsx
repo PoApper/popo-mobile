6893ff98ddf8a96783ec1683f0595088b9c0e39c
BlueHorn07 <bluehornblues@gmail.com>
2025-04-21 17:14:22 +0900

    chore: calendar korean locale

--- src/screens/NewPaxiRoomScreen.tsx
+++ src/screens/NewPaxiRoomScreen.tsx
@@ -15,48 +15,9 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../navigation/types';
 import paxi_api from '../utils/paxi_api';
+import CalendarKoreanLocales from '../utils/calendar-locales';
 
-LocaleConfig.locales.kr = {
-  monthNames: [
-    '1월',
-    '2월',
-    '3월',
-    '4월',
-    '5월',
-    '6월',
-    '7월',
-    '8월',
-    '9월',
-    '10월',
-    '11월',
-    '12월',
-  ],
-  monthNamesShort: [
-    '1월',
-    '2월',
-    '3월',
-    '4월',
-    '5월',
-    '6월',
-    '7월',
-    '8월',
-    '9월',
-    '10월',
-    '11월',
-    '12월',
-  ],
-  dayNames: [
-    '일요일',
-    '월요일',
-    '화요일',
-    '수요일',
-    '목요일',
-    '금요일',
-    '토요일',
-  ],
-  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
-  today: '오늘',
-};
+LocaleConfig.locales.kr = CalendarKoreanLocales;
 LocaleConfig.defaultLocale = 'kr';
 
 type NewPaxiRoomScreenProps = {
