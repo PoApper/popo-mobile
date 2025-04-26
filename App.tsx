3562e61ee012ff37259db072573e4c1ff92777aa
BlueHorn07 <bluehornblues@gmail.com>
2025-04-26 10:30:23 +0900

    feat: firebase try

--- App.tsx
+++ App.tsx
@@ -2,11 +2,41 @@
  * POPO Mobile App
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
+import {
+  requestUserPermission,
+  getFCMToken,
+  onTokenRefresh,
+} from './src/utils/firebase';
 
 const App = () => {
+  useEffect(() => {
+    const initializeFCM = async () => {
+      const hasPermission = await requestUserPermission();
+      if (hasPermission) {
+        const token = await getFCMToken();
+        if (token) {
+          console.log('FCM Token:', token);
+          // TODO: 여기에서 토큰을 서버에 전송
+          // await sendTokenToServer(token);
+        }
+      }
+    };
+
+    initializeFCM();
+
+    // 토큰 갱신 리스너 설정
+    const unsubscribe = onTokenRefresh(newToken => {
+      console.log('New FCM Token:', newToken);
+      // TODO: 여기에서 새로운 토큰을 서버에 전송
+      // await sendTokenToServer(newToken);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <AppNavigator />
--- index.js
+++ index.js
@@ -5,5 +5,20 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+
+// Firebase 백그라운드 메시지 핸들러 등록
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+
+// Firebase 초기화
+messaging()
+  .getInitialNotification()
+  .then(remoteMessage => {
+    if (remoteMessage) {
+      console.log('Notification caused app to open:', remoteMessage);
+    }
+  });
 
 AppRegistry.registerComponent(appName, () => App);
