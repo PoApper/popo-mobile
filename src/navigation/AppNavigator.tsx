4be4c7e9f54cf3d25e67e3392c52c0b2af2ee925
BlueHorn07 <bluehornblues@gmail.com>
2025-05-26 23:08:39 +0900

    chore: organize app nav

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,29 +4,39 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import EncryptedStorage from 'react-native-encrypted-storage';
 
+// Navigation
+import {RootStackParamList} from '@navigation/types';
+import AuthNavigator from '@navigation/AuthNavigator';
+import MainNavigator from '@navigation/MainNavigator';
+
+// Auth Screens
 import LandingScreen from '@screens/LandingScreen';
 import LoginScreen from '@screens/auth/LoginScreen';
 import SignupScreen from '@screens/auth/SignupScreen';
 import UserDetailScreen from '@screens/auth/UserDetailScreen';
 import DeveloperPage from '@screens/auth/DeveloperPage';
-import ReservationScreen from '@screens/ReservationScreen';
+
+// Paxi Screens
 import PaxiRoomListScreen from '@screens/paxi/PaxiRoomListScreen';
 import PaxiCreateRoomScreen from '@screens/paxi/CreatePaxiRoomScreen';
 import NewChatScreen from '@screens/NewChatScreen';
+
+// Place Reservation Screens
 import PlaceReservationScreen from '@screens/place-reservation/PlaceReservationScreen';
 import PlaceDetailReservationScreen from '@screens/place-reservation/PlaceDetailReservationScreen';
 import PlaceReservationApplyScreen from '@screens/place-reservation/PlaceReservationApplyScreen';
-import WhitebookScreen from '@screens/WhitebookScreen';
-import BenefitsScreen from '@screens/BenefitsScreen';
+
+// Club & Association Screens
 import ClubScreen from '@screens/club/ClubScreen';
 import ClubDetailScreen from '@screens/club/ClubDetailScreen';
 import AssociationScreen from '@screens/association/AssociationScreen';
 import AssociationDetailScreen from '@screens/association/AssociationDetailScreen';
-import CampusShuttleScreen from '@screens/CampusShuttle';
 
-import {RootStackParamList} from '@navigation/types';
-import AuthNavigator from '@navigation/AuthNavigator';
-import MainNavigator from '@navigation/MainNavigator';
+// Other Screens
+import ReservationScreen from '@screens/ReservationScreen';
+import WhitebookScreen from '@screens/WhitebookScreen';
+import BenefitsScreen from '@screens/BenefitsScreen';
+import CampusShuttleScreen from '@screens/CampusShuttle';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -60,37 +70,27 @@ const AppNavigator = () => {
           screenOptions={{
             headerShown: false,
           }}>
-          {/* <Stack.Screen name="PaxiStart" component={PaxiStartScreen} /> */}
-          {/* <Stack.Screen name="Chat" component={ChatScreen} /> */}
-          <Stack.Screen name="NewChat" component={NewChatScreen} />
-          {/* <Stack.Screen name="Settlement" component={SettlementScreen} /> */}
+          {/* Auth Screens */}
+          <Stack.Screen name="Landing" component={LandingScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen name="UserDetail" component={UserDetailScreen} />
+          <Stack.Screen name="Developer" component={DeveloperPage} />
+
+          {/* Paxi Screens */}
           <Stack.Screen name="PaxiRoomList" component={PaxiRoomListScreen} />
           <Stack.Screen
             name="CreatePaxiRoomScreen"
             component={PaxiCreateRoomScreen}
           />
-          <Stack.Screen name="Landing" component={LandingScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="UserDetail" component={UserDetailScreen} />
-          <Stack.Screen name="Developer" component={DeveloperPage} />
+          <Stack.Screen name="NewChat" component={NewChatScreen} />
+
+          {/* Place Reservation Screens */}
           <Stack.Screen name="Reservation" component={ReservationScreen} />
           <Stack.Screen
             name="PlaceReservation"
             component={PlaceReservationScreen}
           />
-          <Stack.Screen name="Signup" component={SignupScreen} />
-          <Stack.Screen name="Whitebook" component={WhitebookScreen} />
-          <Stack.Screen name="Benefits" component={BenefitsScreen} />
-          <Stack.Screen name="Club" component={ClubScreen} />
-          <Stack.Screen name="ClubDetail" component={ClubDetailScreen} />
-          <Stack.Screen name="Association" component={AssociationScreen} />
-          <Stack.Screen
-            name="AssociationDetail"
-            component={AssociationDetailScreen}
-          />
-          <Stack.Screen name="CampusShuttle" component={CampusShuttleScreen} />
-          <Stack.Screen name="Auth" component={AuthNavigator} />
-          <Stack.Screen name="Main" component={MainNavigator} />
           <Stack.Screen
             name="PlaceDetailReservation"
             component={PlaceDetailReservationScreen}
@@ -99,6 +99,24 @@ const AppNavigator = () => {
             name="PlaceReservationApply"
             component={PlaceReservationApplyScreen}
           />
+
+          {/* Club & Association Screens */}
+          <Stack.Screen name="Club" component={ClubScreen} />
+          <Stack.Screen name="ClubDetail" component={ClubDetailScreen} />
+          <Stack.Screen name="Association" component={AssociationScreen} />
+          <Stack.Screen
+            name="AssociationDetail"
+            component={AssociationDetailScreen}
+          />
+
+          {/* Other Screens */}
+          <Stack.Screen name="Whitebook" component={WhitebookScreen} />
+          <Stack.Screen name="Benefits" component={BenefitsScreen} />
+          <Stack.Screen name="CampusShuttle" component={CampusShuttleScreen} />
+
+          {/* Main Navigators */}
+          <Stack.Screen name="Auth" component={AuthNavigator} />
+          <Stack.Screen name="Main" component={MainNavigator} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
