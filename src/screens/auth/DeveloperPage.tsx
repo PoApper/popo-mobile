7d0c1e03f88154267bb62224dca13999862b65d3
Seokyun Ha <bluehornblues@gmail.com>
2025-08-30 23:42:10 +0900

    Revert "setup: react native config (#126)" (#127)
    This reverts commit 72bb2fe38fd6ece1e12d5ce2dba3f3a54d39ac2e.

--- src/screens/auth/DeveloperPage.tsx
+++ src/screens/auth/DeveloperPage.tsx
@@ -19,8 +19,8 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import {RootStackParamList} from '@navigation/types';
 import {getAuthToken} from '@utils/auth-token';
 import api from '@utils/api';
+import paxi_api from '@utils/paxi_api';
 import CommonHeader from '@components/CommonHeader';
-import paxi_api, {PAXI_API_URL} from '@utils/paxi_api';
 import {POPO_API_URL} from '@utils/api';
 import Environment from '@utils/environment';
 
@@ -141,8 +141,6 @@ const DeveloperPage = ({navigation}: DeveloperPageProps) => {
                   style={[styles.tokenValue, {color: textColor}]}
                   selectable={true}>
                   {POPO_API_URL || '정보 없음'}
-                  {'\n'}
-                  {PAXI_API_URL || '정보 없음'}
                 </Text>
               </View>
             </View>
