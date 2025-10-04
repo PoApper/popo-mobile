dc037e542a71b4fd505db634d78fee3eadf9d67d
Gwanho Kim <khkim6040@gmail.com>
2025-10-04 23:47:18 +0900

    Fix: Override onCreate function not to pass saved state (#189)
    * Fix: Override onCreate function not to pass saved state
    
    * Update: Set fragmentFactory before onCreate()
    
    Fabric 아키텍쳐 사용하고 있으므로 onCreate() 전 해당 라인 호출해야 함. https://www.npmjs.com/package/react-native-screens 의 Kotlin 부분 참고
    
    * Comment: Add official docs
    
    * Chore: Update current versioning
    
    * Revert "Update: Set fragmentFactory before onCreate()"
    
    react-native-screens 버전이 최신과 달라 해당 함수 지원안해서 삭제
    
    * Chore: Add further todo jobs

--- android/app/src/main/java/com/popomobile/MainActivity.kt
+++ android/app/src/main/java/com/popomobile/MainActivity.kt
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +20,14 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  // react-native-screens와 겹쳐서 Android OS가 앱을 다시 로드할 때 이전 상태를 보내지 않게 함
+  // 관련 이슈: https://github.com/software-mansion/react-native-screens/issues/1481
+  // 공식문서: https://www.npmjs.com/package/react-native-screens#Installation 의 Kotlin 부분 코드 참고
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // react-native-screens 버전 4.16 이상으로 올린다면 아래 Fabric 반영한 코드 추가해 줘야 함
+    // import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory;
+    // getSupportFragmentManager().setFragmentFactory(new RNScreensFragmentFactory());
+    super.onCreate(null)
+  }
 }
