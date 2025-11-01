3139ec12cc250930af8517e9eb4d30a7d3d2471c
Gwanho Kim <khkim6040@gmail.com>
2025-11-01 21:02:54 +0900

    Refactor: try-catch

--- src/utils/reset.ts
+++ src/utils/reset.ts
@@ -2,17 +2,21 @@ import CookieManager from '@react-native-cookies/cookies';
 import EncryptedStorage from 'react-native-encrypted-storage';
 
 export async function reset_auth() {
-  if (await EncryptedStorage.getItem('auth_token')) {
-    await EncryptedStorage.removeItem('auth_token');
+  try {
+    if (await EncryptedStorage.getItem('auth_token')) {
+      await EncryptedStorage.removeItem('auth_token');
+    }
+    if (await EncryptedStorage.getItem('refresh_token')) {
+      await EncryptedStorage.removeItem('refresh_token');
+    }
+    if (await EncryptedStorage.getItem('isAuthenticated')) {
+      await EncryptedStorage.removeItem('isAuthenticated');
+    }
+    if (await EncryptedStorage.getItem('user_info')) {
+      await EncryptedStorage.removeItem('user_info');
+    }
+    await CookieManager.clearAll();
+  } catch (error) {
+    console.error('인증 정보 초기화 오류:', error);
   }
-  if (await EncryptedStorage.getItem('refresh_token')) {
-    await EncryptedStorage.removeItem('refresh_token');
-  }
-  if (await EncryptedStorage.getItem('isAuthenticated')) {
-    await EncryptedStorage.removeItem('isAuthenticated');
-  }
-  if (await EncryptedStorage.getItem('user_info')) {
-    await EncryptedStorage.removeItem('user_info');
-  }
-  await CookieManager.clearAll();
 }
