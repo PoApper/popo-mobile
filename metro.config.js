a9df8f167db9d0607a271603caeca1c32fd9e137
Gwanho Kim <khkim6040@gmail.com>
2026-03-02 15:53:59 +0900

    fix: Metro package exports 활성화로 axios 번들 resolve 수정
    axios 1.13.x에서 main 필드가 Node.js 번들을 가리키면서
    Metro가 crypto 모듈을 찾지 못하는 문제 발생.
    unstable_enablePackageExports를 활성화하여 exports 필드의
    react-native 조건으로 브라우저 번들이 resolve되도록 수정.

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,11 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    // axios 1.13.x 업그레이드 후 exports 필드의 react-native 조건으로 브라우저 번들을 resolve하기 위해 필요. 제거 시 crypto 모듈 에러 발생.
+    unstable_enablePackageExports: true,
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
